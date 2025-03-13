81575267f64fd207ef8163cc73a422bfaf5e6a40
Grant Bossa <Grantbossa@westernwyoming.edu>
2025-03-13 23:59:26 +0000

    In Progress

--- ackermanns-function/main.py
+++ ackermanns-function/main.py
@@ -4,5 +4,28 @@
 # Ackermann's Function Programming Project
 # SDEV 1200
 #
-
-# Use comments liberally throughout the program. 
+'''
+`If m = 0 then return n + 1 
+If n = 0 then return ackermann(m - 1, 1) 
+Otherwise, return ackermann(m - 1, ackermann(m, n - 1))`
+'''
+def A(m,n):
+    if m == 0 :
+        return n + 1 
+    if n == 0 :
+        return A(m - 1, 1) 
+    else:
+        return A(m - 1, A(m, n - 1))
+ 
+def main():
+    # Initial Variables
+    try :
+        m = int(input("Please enter the m value: "))
+        n = int(input("Please enter the n value: "))
+        z = A(m, n)
+        print(f"The Ackermann value is {z}")
+    catch
+        
+# Call the main function ONLY if the file is being run as a standalone program.
+if __name__ == "__main__":
+    main()
--- recursive-multiplication/main.py
+++ recursive-multiplication/main.py
@@ -1,7 +1,32 @@
 #
-# Name
-# Date
+# Grant Bossa
+# March 13, 2025
 # Recursive Multiplication Programming Project
 # SDEV 1200
 #
-# Use comments liberally throughout the program. 
+# Use comments liberally throughout the program.
+
+
+def recurse(x, y):
+    if x == 0 or y == 0:
+        return 0
+    if x == 1:
+        return y
+    if y == 1:
+        return x
+    if x > 1:
+        return y + recurse(x - 1, y)
+
+
+def main():
+    # Initial Variables
+
+    x = int(input("Please enter the X value: "))
+    y = int(input("Please enter the Y value: "))
+    z = recurse(x, y)
+    print(f"The value is {z}")
+
+
+# Call the main function ONLY if the file is being run as a standalone program.
+if __name__ == "__main__":
+    main()
