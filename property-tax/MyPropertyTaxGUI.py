36e90e286ec0279b9a28cf86ca0f24f30dd08b4f
Grant A. Bossa <Grantbossa@westernwyoming.edu>
2025-04-05 20:08:16 +0000

    Ready for Grading

--- property-tax/MyPropertyTaxGUI.py
+++ property-tax/MyPropertyTaxGUI.py
@@ -5,30 +5,29 @@
 # SDEV 1200
 #
 
-# Instructions  
+# Instructions
 '''
-A county collects property taxes on the assessment value of property, which is 60 percent of the property�s actual value. 
-If an acre of land is valued at $10,000, its assessment value is $6,000. 
-The property tax is then $0.75 for each $100 of the assessment value. 
-The tax for the acre assessed at $6,000 will be $45.00. 
+A county collects property taxes on the assessment value of property, which is 60 percent of the property's actual value.
+If an acre of land is valued at $10,000, its assessment value is $6,000.
+The property tax is then $0.75 for each $100 of the assessment value.
+The tax for the acre assessed at $6,000 will be $45.00.
 Write a GUI program that displays the assessment value and property tax when a user enters the actual value of a property.
 '''
 
 import tkinter as tk
+
 
 class MyGUI:
    def __init__(self):
       # Create the main window.
       self.main_window = tk.Tk()
       self.main_window.title("Property Tax Valuation")
-      self.main_window.geometry("250x200")
+      self.main_window.geometry("300x150")
 
-      # Create StringVar objects to display 
-      # name, street, and city-state-zip
-      text_variable = tk.StringVar()
-
+      # Create StringVar objects to display
+      self.text_entry_variable = tk.StringVar()
       self.property_value = tk.StringVar()
-      self.assesment_value = tk.StringVar()
+      self.assessment_value = tk.StringVar()
       self.property_tax_value = tk.StringVar()
 
       # Create three frames.
@@ -37,29 +36,36 @@ class MyGUI:
       self.info_frame = tk.Frame(self.main_window)
 
       # Create Entry widgets
-      my_entry = tk.Entry(self.entry_frame, textvariable=text_variable)
-      text_variable.set("Enter Property Value Here")
+      self.L1 = tk.Label(self.entry_frame, text="Enter Property Value")
+      self.my_entry = tk.Entry(
+         self.entry_frame, bd=5, textvariable=self.text_entry_variable
+      )
+      self.text_entry_variable.set("")
 
       # Pack the Entry widgets
-      my_entry.pack()
+      self.L1.pack(side="left")
+      self.my_entry.pack()
 
       # Create the button widgets.
-      self.calculate_button = tk.Button(self.button_frame, text = "Calculate Values", command = self.calculateValues)
-      self.quit_button = tk.Button(self.button_frame, text = "Quit", command = self.main_window.destroy)
+      self.calculate_button = tk.Button(
+         self.button_frame, text="Calculate Values", command=self.calculateValues
+      )
+      self.quit_button = tk.Button(
+         self.button_frame, text="Quit", command=self.main_window.destroy
+      )
 
       # Pack the buttons.
       self.calculate_button.pack(side="left")
-      self.quit_button.pack(side='right')      
+      self.quit_button.pack(side="right")
 
       # Create the label widgets associated with the StringVar objects.
-      self.property_label = tk.Label(self.info_frame, textvariable=self.property_value)
-      self.assessment = tk.Label(self.info_frame, textvariable=self.assesment_value)
-      self.property_tax_label = tk.Label(self.info_frame, textvariable=self.property_tax_value)
-
-      # Pack the labels.
-      self.property_label.pack()
-      self.assessment.pack()
-      self.property_tax_label.pack()
+      self.property_label = tk.Label(
+         self.info_frame, textvariable=self.property_value
+      )
+      self.assessment = tk.Label(self.info_frame, textvariable=self.assessment_value)
+      self.property_tax_label = tk.Label(
+         self.info_frame, textvariable=self.property_tax_value
+      )
 
       # Pack the frames.
       self.entry_frame.pack()
@@ -70,14 +76,31 @@ class MyGUI:
       tk.mainloop()
 
    def calculateValues(self):
-      self.property_value.set(my_entry.get())
-      self.assesment_value.set("Assessment Value")
-      self.property_tax_value.set("Property Tax Value")
+      self.float_property_value = float(self.my_entry.get())
+      self.float_assessment_value = float(self.float_property_value) * 0.6
+      self.float_property_tax_value = float(self.float_assessment_value) * 0.0075
+
+      self.property_value.set(
+         f"{"Property Value":<24} $ {self.float_property_value:>10.2f}"
+      )
+      self.assessment_value.set(
+         f"{"Assessment Value":<20} $ {self.float_property_value * 0.6:>10.2f}"
+      )
+      self.property_tax_value.set(
+         f"{"Property Tax":<25} $ {self.float_assessment_value * .0075:>10.2f}"
+      )
+
+      # Pack the labels.
+      self.property_label.pack(anchor="w")
+      self.assessment.pack(anchor="w")
+      self.property_tax_label.pack(anchor="w")
+
 
 def main():
-    # Create an instance of the MyGUI class.
-    my_gui = MyGUI()
+   # Create an instance of the MyGUI class.
+   my_gui = MyGUI()
+
 
 # Call the main function ONLY if the file is being run as a standalone program.
-if __name__ == '__main__' :
-    main()
+if __name__ == "__main__":
+   main()
--- property-tax/main.py
+++ property-tax/main.py
@@ -5,20 +5,22 @@
 # SDEV 1200
 #
 
-# Instructions  
-'''
-A county collects property taxes on the assessment value of property, which is 60 percent of the property�s actual value. 
-If an acre of land is valued at $10,000, its assessment value is $6,000. 
-The property tax is then $0.75 for each $100 of the assessment value. 
-The tax for the acre assessed at $6,000 will be $45.00. 
+# Instructions
+"""
+A county collects property taxes on the assessment value of property, which is 60 percent of the property's actual value.
+If an acre of land is valued at $10,000, its assessment value is $6,000.
+The property tax is then $0.75 for each $100 of the assessment value.
+The tax for the acre assessed at $6,000 will be $45.00.
 Write a GUI program that displays the assessment value and property tax when a user enters the actual value of a property. '
-'''
+"""
 import MyPropertyTaxGUI
+
 
 def main():
     # Create an instance of the MyGUI class.
     my_gui = MyPropertyTaxGUI.MyGUI()
 
+
 # Call the main function ONLY if the file is being run as a standalone program.
-if __name__ == '__main__' :
+if __name__ == "__main__":
     main()
